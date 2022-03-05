ff8cd7d0c6c7ca4436969d6da931b863d19be3be
Jamen1147 <jamen1147@gmail.com>
2022-03-06 09:33:11 +1100

    feat: remove color group typing

--- packages/components/src/style/color/index.ts
+++ packages/components/src/style/color/index.ts
@@ -1,10 +1,2 @@
-export type ColorGroup = {
-  100?: string;
-  200?: string;
-  300?: string;
-  400?: string;
-  500?: string;
-};
-
 export { default as primary } from './primary';
 export { default as secondary } from './secondary';
